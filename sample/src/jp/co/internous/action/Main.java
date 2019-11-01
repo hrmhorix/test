574a3eaa4c054c6565c4c4039abd0b2c919b27d3
hrmhorix <pize430@gmail.com>
2019-11-01 20:34:14 +0900

    11/1

---
+++ sample/src/jp/co/internous/action/Main.java
@@ -0,0 +1,20 @@
+package jp.co.internous.action;
+
+public class Main {
+
+
+	public static void main (String[] args){
+		System.out.println("Hello World");
+		System.out.println(circle(5));
+
+	}
+	public static int gokei(){
+		return 1+1;
+	}
+	public static int gokei2(int number1,int number2){
+		return number1 + number2;
+	}
+	public static double circle(int hankei){
+		return hankei * hankei * 3.14;
+	}
+}
