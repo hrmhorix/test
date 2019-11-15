4886cb63ddf3e5159ba764122df0fc9bcb136455
hrmhorix <haru.isk@gmail.com>
2019-11-15 21:24:27 +0900

    11/15

---
+++ Capsule/src/Capsule.java
@@ -0,0 +1,23 @@
+
+public class Capsule {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Person taro = new Person("山田太郎",20);
+		System.out.println(taro.getName());
+		
+		taro.setName("花子");
+		taro.setterAge(30);
+System.out.println();
+		
+		
+		System.out.println(taro.getName() + " " + taro.getterAge());
+		
+
+
+		
+
+	}
+
+}
