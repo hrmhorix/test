484711a297f2c375df5015b9441bc43eafba9dc4
obafgkm44 <momo0811toto@gmail.com>
2019-11-25 21:39:09 +0900

    11/25

--- TestDB/sql/sampledb.sql
+++ TestDB/sql/sampledb.sql
@@ -2,14 +2,14 @@ drop database if exists sampledb;
 create database sampledb;
 use sampledb;
 
-/*create table sample_table(
-user_no int,
-user_name varchar(50),
-user_age int,
+create table sample_test(
+No int,
+name varchar(50),
+age int,
 mail varchar(50)
 );
 
-insert into sample_table value(1,"Yamada",21,"xxx@yahoo.co.jp");
-insert into sample_table value(2,"Sato",39,"yyy@gmail.com");
-insert into sample_table value(3,"Harada",44,"zzz@yahoo.co.jp");
-*/
+
+insert into sample_test value(1,"Yamada",21,"xxx@yahoo.co.jp");
+insert into sample_test value(2,"Sato",39,"yyy@google.com");
+insert into sample_test value(3,"Harada",44,"zzz@yahoo.co.jp");
